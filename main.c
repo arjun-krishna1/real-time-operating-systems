c70c55cbc15d26cc29d1ba1a4d683aa7054f20ae
Andrei Ikic <a2ikic@uwaterloo.ca>
2021-11-02 19:22:48 -0400

    Add threads and initialization of threads

--- main.c
+++ main.c
@@ -3,134 +3,141 @@
 		Andrei Ikic (a2ikic@uwaterloo.ca)
 */
 
-#include <cmsis_os2.h>
+#include <cmsis_os2.h> // include RTOS functions
 #include <LPC17xx.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
+#define N 2
+
+__NO_RETURN void client(osMessageQueueId_t queue_id)
+{
+	while(1)
+	{	
+		printf("client\n");
+		osThreadYield();
+	}
+}
+
+__NO_RETURN void server(osMessageQueueId_t queue_id)
+{
+	while(1)
+	{
+		printf("server\n");
+		osThreadYield();
+	}
+}
+
+__NO_RETURN void monitor(osMessageQueueId_t queue_id)
+{
+	printf("entered monitor\n");
+	while(1)
+	{
+		printf("monitor\n");
+		osThreadYield();
+	}
+}
+
 int main (void)
 {
-	printf("Hello World!\n");
-	return 0;
+	SystemCoreClockUpdate(); 
+	osKernelInitialize();
+
+	// create a new message queue
+	osMessageQueueId_t messageQueues [N];
 	
+	// create message queues
+	for(int i = 0; i < N; i++)
+		messageQueues[i] = osMessageQueueNew(10, sizeof(int), NULL);
+	
+	// create client and serverthreads
+	for(int i = 0; i < N; i ++)
+	{
+		osThreadNew(client, messageQueues[i], NULL);
+		osThreadNew(server, messageQueues[i], NULL);
+	}
+	
+	osThreadNew(monitor, NULL, NULL);
+	printf("starting kernels\n");
+	osKernelStart();
+	return 0;
 }
 
 /*
-void setupLEDS (void)
-{
-	// set these LED's as output
-	LPC_GPIO1->FIODIR |= 0xB0000000; // bit 28,29, 31 on GPIO1
-	LPC_GPIO2->FIODIR |= 0x0000007C; // bit 2,3,4,5,6 on GPIO2
-} 
-
-void setupADC (void)
-{
-	LPC_SC->PCONP |= 1u << 12; // enable ADC power/clock control bit
-	LPC_ADC->ADCR |= 1 << 21; // PDN = 1 --> ADC is operational
-	LPC_SC->PCLKSEL0 &= ~(00u << 24); // divide cclk by 4
-
-	LPC_PINCON->PINSEL1 &= ~(1u << 19); // set pin function to AD0.2
-	LPC_PINCON->PINSEL1 |= 1u << 18;
+// create a new message queue with space for 10 messages
+	// each of which is an integer
+	// 3rd param, pointer to a struct defining message queue attrs
+	// q_id: message queue identifier
+	// use q_id in future get and put operations
+	osMessageQueueId_t q_id = osMessageQueueNew(10, sizeof(int), NULL);
+	int msg = 7;
+	// use the q_id queue, store msg as a message
+	// 3rd param is the priority
+	// 4th param is timout
+	// returns osErrorResource if not enough space in queue
+	// put: share message in queue
+	osMessageQueuePut(q_id, &msg, 0, 0);
 	
-	LPC_ADC->ADCR &= ~(0xff); // clear bits 0 - 7 before setting bit 2
-	LPC_ADC->ADCR |= 1u << 2; // select bit 2 so AD0.2 is sampled
-	LPC_ADC->ADCR |= 0x03 << 8; // set clock division by (3+1)
-	LPC_ADC->ADCR &= ~(0 << 16); // burst  bit is 0
-	LPC_ADC->ADCR |= 1 << 24; // set START bits to initiate conversion
-}
-
-__NO_RETURN void printADC(void *arg) // ADC thread
-{
-	while(1)
-	{
-		if(LPC_ADC->ADGDR & (1u << 31)) 
-		{
-			// obtain bits 4 to 15 in ADGDR and convert
-			float a = ((LPC_ADC->ADGDR & 0xfff0) >> 4) * (3.3 / (pow(2, 12) - 1));
-			printf("%.1f\n", a);
-			
-			LPC_ADC->ADCR |= 1 << 24; // set START bits to initiate conversion
-			osThreadYield(); // give other threads a chance to work
-		}
-	}
-}
-
-__NO_RETURN void joystickLED (void *arg) // Joystick, LED'S thread
-{
-	while(1)
-	{
-		LPC_GPIO2->FIOCLR |= 0x0000007C; // clear 5 rightmost LEDs 
-		
-		if(!(LPC_GPIO1->FIOPIN & (1u << 20))) // check if joystick is pressed
-		{
-			LPC_GPIO2->FIOSET |= (1 << 2);
-		}
-		else
-		{
-			LPC_GPIO2->FIOCLR |= (1 << 2);
-		}
-		
-		if(!(LPC_GPIO1->FIOPIN & (1u << 23))) // check north
-		{
-			LPC_GPIO2->FIOSET |= (1 << 6);
-		}
-		
-		else if(!(LPC_GPIO1->FIOPIN & (1u << 24))) // check east
-		{
-			LPC_GPIO2->FIOSET |= (1 << 5);
-		}
-		
-		else if(!(LPC_GPIO1->FIOPIN & (1u << 25))) // check south
-		{
-			LPC_GPIO2->FIOSET |= (1 << 6) | (1 << 5);
-		}
-		
-		else if(!(LPC_GPIO1->FIOPIN & (1u << 26))) // check west
-		{
-			LPC_GPIO2->FIOSET |= (1 << 4);
-		}
-		
-		osThreadYield();
-	}	
-}
-
-
-__NO_RETURN void pushButton (void *arg)
-{ // active low button
-	while (1)
-	{
-		if (!(LPC_GPIO2->FIOPIN & (1u << 10))) // wait for button to be pressed (low)
-		{
-			while (!(LPC_GPIO2->FIOPIN & (1u << 10)) ); // wait for button to be released after press
-			
-			if (LPC_GPIO1->FIOPIN & (1u << 28)) // switch state of LED
-				LPC_GPIO1->FIOCLR |= (1u << 28);
-			else
-				LPC_GPIO1->FIOSET |= (1u << 28);
-		}
-		osThreadYield();
-	}
-}
-
-
-int main (void)
-{
-	printf("radical\n");
-	setupLEDS();
-	setupADC();
+	msg = 0;
+	// get: recieve message from queue
+	// receive a message from a queue with blocking indefinitely
+	// 2nd param : points to a buffer (&msg)
+	// buffer: temp place to store data
+	// 	the kernel copies the message to
+	// 3rd paramater: records message priority, NULL discards it
+	// 4th: timeout, osWaitForever, blocks the calling thread
+	// until message is retrieved
+	osMessageQueueGet(q_id, &msg, NULL, osWaitForever);
 	
-	SystemCoreClockUpdate(); 
-	osKernelInitialize();
+	// create a total of 2*N + 1 concurrent threads
+	// N clients, N servers (one for each queue)
+	// one monitor thread, displays system status on the serial port
 	
-	// inform RTOS of threads to run
-	osThreadNew(printADC, NULL, NULL);
-	osThreadNew(joystickLED, NULL, NULL);
-	osThreadNew(pushButton, NULL, NULL);
+	// each client thread starts by determining which
+	// queue it is sending messages to
+	// waits a random period of time
+	// sends a messge to one queue
+	// GOTO wait
 	
-	// start kernel
-	osKernelStart();
+	// server:
+	// determining which queue it is watching
+	// wait a random amount of time
+	// retrieve a message from queue
+	// GOTO wait
+	// wait: simulate service time
 	
-}
+	// have one server function that is used to create N server threads
+	// each with a parameter, queue id
+	// don't have N separate server functions
+	// one client function, create N client threads
+	// parameter that specifies what queue to send messages to
+	
+	MONITOR THREAD
+	runs once a second
+	prints the following info for each of the N queues to UART
+	1. elapsed time in seconds
+	2. total number of messages sent
+	3. total number of messages recieved
+	4. total number of overflows
+		osMessageQueuePut returns error
+	5. current number of messages in the queue
+		use osMessageQueueGetCount
+	6. Average message loss ratio
+		number of overflows divided by total messages sent
+	7. average message arrival rate (total number of messages sent / elapsed time)
+	8. average service rate (total number of recieved messages / sum of server random sleep time)
+
+	TODO
+	create array of size N, to store queue id's
+	create N message queues, and store queue id's in an array
+	create N client threads
+	create N server threads
+	DONE
+	client:
+		determine which queue you are sending messages to
+		wait a random period of time
+		send a message to the queue
+		GOTO wait
+		
 */
-
